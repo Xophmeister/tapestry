1d73b3cd7193491db4513071e1ec431f90dcbd67
Christopher Harrison <github@acc.xoph.co>
2013-10-22 23:31:40 +0100

    Hack hack hack!

--- Main.hs
+++ Main.hs
@@ -3,7 +3,19 @@ module Main where
 import System.Environment (getArgs)
 import Codec.Picture
 
+showDetails :: DynamicImage -> IO ()
+showDetails (ImageRGBA8 img) = do
+  print $ imageData img
+--showDetails img = promoteImage img
+showDetails _ = print "Unhandled colourspace"
+
 main :: IO ()
 main = do
   argv <- getArgs
-  print $ show argv
+  case argv of
+    [] -> putStrLn "No arguments"
+    (filename : _) -> do
+      dynImage <- readImage filename
+      case dynImage of
+        Left  err -> putStrLn err
+        Right img -> showDetails img
