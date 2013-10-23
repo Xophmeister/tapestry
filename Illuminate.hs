b3259d49d7b792c6c456c4de22154fd7a1383256
Christopher Harrison <github@acc.xoph.co>
2013-10-23 13:12:36 +0100

    Attempting to convert to RGB8

--- Illuminate.hs
+++ Illuminate.hs
@@ -1,7 +1,11 @@
 module Illuminate where
 
 import Codec.Picture
+import Codec.Picture.Types
 
-toLuminance :: DynamicImage -> Either String String
-toLuminance (ImageRGBA8 img) = Right (show $ imageData img)
-toLuminance _ = Left "Unhandled colourspace"
+toRGB8 :: DynamicImage -> Image PixelRGBA8
+toRGB8 (ImageRGB8  img) = promoteImage img
+toRGB8 (ImageRGBA8 img) = img
+
+toLuminance :: DynamicImage -> String
+toLuminance image = show $ imageData $ toRGB8 image
