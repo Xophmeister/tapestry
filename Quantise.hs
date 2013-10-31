8ec11c57c66e5fcf2298adc636ef6f49251f5d28
Christopher Harrison <github@acc.xoph.co>
2013-10-31 23:45:39 +0000

    I don't know what I'm doind :( This works, but it's messy

--- Quantise.hs
+++ Quantise.hs
@@ -1,35 +1,34 @@
-{-# LANGUAGE FlexibleInstances #-}
 module Quantise (quantise) where
 
+import Data.List
+import Data.Ord
 import Codec.Picture.Types
 import Image (ImageData(..), ColourStream(..))
 import Palette (Palette(..))
 
--- n-dimensional Euclidean metric
--- We need to find the distance between colours
--- n.b., Pixel8 is a synonym for Word8, so we need FlexibleInstances
--- Also Word8 wraps on overflow, so we need explicit fromIntegral
+-- n-dimensional Euclidean metric, to find the "distance" between colours
+-- n.b., Word8 wraps on overflow, so we need explicit fromIntegral
 euclidean :: Integral a => [a] -> [a] -> Double
 euclidean a b = sqrt . sum $ zipWith (\u v -> (u - v)^2) x y
                 where x = map fromIntegral a
                       y = map fromIntegral b
 
-class Colour a where
-  d :: a -> a -> Double
-  extractStreamData :: ColourStream -> [a]
-  buildColourStream :: [a] -> ColourStream
+quantise :: Palette -> ImageData -> ImageData
 
-instance Colour Pixel8 where
-  d a b = euclidean [a] [b]
-  extractStreamData (Greys x) = x
-  buildColourStream = Greys
+quantise (Palette _ (Greys   paletteStream)) (ImageData w h (Greys   imgStream)) =
+  ImageData w h $ Greys (nearestNeighbourGrey paletteStream imgStream)
 
-instance Colour PixelRGB8 where
-  d (PixelRGB8 r1 g1 b1) (PixelRGB8 r2 g2 b2) = euclidean [r1, g1, b1] [r2, g2, b2]
-  extractStreamData (Colours x) = x
-  buildColourStream = Colours
+quantise (Palette _ (Colours paletteStream)) (ImageData w h (Colours imgStream)) =
+  ImageData w h $ Colours (nearestNeighbourRGB paletteStream imgStream)
 
 -- Nearest-neighbour quantisation
 -- TODO Probabilistic nearest-neighbour...
-quantise :: Palette -> ImageData -> ImageData
-quantise (Palette _ pStream) (ImageData w h imgStream) = undefined
+nearestNeighbourGrey :: [Pixel8] -> [Pixel8] -> [Pixel8]
+nearestNeighbourGrey palette = 
+  map (\c -> snd (c, fst $ minimumBy (comparing snd) $ map (\x -> (x, d c x)) palette))
+  where d a b = euclidean [a] [b]
+
+nearestNeighbourRGB :: [PixelRGB8] -> [PixelRGB8] -> [PixelRGB8]
+nearestNeighbourRGB palette = 
+  map (\c -> snd (c, fst $ minimumBy (comparing snd) $ map (\x -> (x, d c x)) palette))
+  where d (PixelRGB8 r1 g1 b1) (PixelRGB8 r2 g2 b2) = euclidean [r1, g1, b1] [r2, g2, b2]
